28d1974819335f00b0fbd1764efe23e815969058
bridleiva <wham-stride-sulk@duck.com>
2025-09-14 02:35:41 -0600

    compilado el formato inicial

---
+++ devenv.nix
@@ -0,0 +1,62 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ 
+    git entr findutils 
+    ];
+
+  # https://devenv.sh/languages/
+  languages.texlive = {
+    enable = true;
+    packages = with pkgs.texlive;
+      [ 
+        "babel-spanish" "physics" "amsfonts"
+        "siunits" 
+        
+        ### Estos son los paquetes necesarios para
+        ### compilar el formato de Elsevier
+        "elsarticle" "lipsum"
+       ];
+  };
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.watch-latex.exec = ''
+    find . -name "*.tex" -or -name "*.bib" \
+    | entr -s "pdflatex -interaction=nonstopmode grb.tex && \
+     bibtex grb && \
+     pdflatex -interaction=nonstopmode grb.tex && \
+     pdflatex -interaction=nonstopmode $MAIN_NAME && \
+     echo 'Compilation completed.'"
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
